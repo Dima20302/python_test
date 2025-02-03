a27a68ce3004e6fcf06021649016fad6b49887ec
Dima20302 <20302stf@gmail.com>
2025-02-03 14:26:57 +0300

    timer sec

---
+++ repeat1.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+import time
+
+def update_time():
+    elapsed_time = time.time() - start_time
+    minutes = int(elapsed_time // 60)
+    seconds = int(elapsed_time % 60)
+    time_label.config(text=f"{minutes:02}:{seconds:02}")
+    root.after(1000, update_time)
+
+def start_timer():
+    global start_time
+    start_time = time.time()
+    update_time()
+
+root = tk.Tk()
+root.title("Секундомер")
+root.geometry("200x100")
+root.configure(bg="lightgreen")
+root.attributes("-topmost", True)
+
+time_label = tk.Label(root, text="00:00", font=("Helvetica", 48), fg="red", bg="lightgreen")
+time_label.pack(pady=20)
+
+start_button = tk.Button(root, text="Старт", command=start_timer)
+start_button.pack(pady=10)
+
+root.mainloop()
